376b02f4bd6a05fce5758d51e0b72b8c5301eb25
lizwachira <lizwanguwachira@gmail.com>
2024-04-09 05:14:58 +0300

    functionscontinued

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+//=> should return a value string
+const saturdayFun = (activity = 'roller-skate') => 
+`This Saturday, I want to ${activity}!`;
+
+const mondayWork = (activity = 'go to the office') => 
+`This Monday, I will ${activity}.`;
+
+const wrapAdjective = (style = '*') => (adjective = 'dedicated') => 
+`You are ${style}${adjective}${style}!`;
+const bold = wrapAdjective('**');
+
+//output examples of functions with different input values and output strings 
+console.log(saturdayFun()); 
+console.log(mondayWork());
+console.log(bold()); 
+console.log(bold('programmer'));
